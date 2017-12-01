b96d1fc841ee7591e677db12c1dfd8815b50b4bb
Ben Secretan <github@benjsec.33mail.com>
2017-12-01 11:40:36 +0000

    Test code for all examples given@

--- 2017/01/part1.nim
+++ 2017/01/part1.nim
@@ -1,26 +1,29 @@
-var inp: seq[int]
+from strutils import parseInt
 
 type 
-    Test = tuple[inp: seq[int], outp: int]
+    Test = tuple[inp: string, outp: int]
 
-var
-    t1: Test
-t1 = (inp: @[1, 1, 2, 2], outp: 3)
+var tests: seq[Test]
 
-inp = @[1, 1, 2, 2]
-let expected = 3
+tests = @[]
+tests.add((inp: "1122", outp: 3))
+tests.add((inp: "1111", outp: 4))
+tests.add((inp: "1234", outp: 0))
+tests.add((inp: "91212129", outp: 9))
 
-proc sum_match(list: seq): int =
-    var outp = 0
+proc sum_match(list: string): int =
+    # Return the sum of digits in a string where the digit
+    # matches the following digit.
     for idx in list.low()..<list.high():
         if list[idx] == list[idx+1]:
-            outp+=list[idx]
+            # This seems a really long way around...
+            result += int(list[idx])-int('0')
     if list[0] == list[high(list)]:
-        outp+=list[0]
-    return outp
+        result += int(list[0])-int('0')
 
-if sum_match(t1.inp) == t1.outp:
-  echo "PASS"
-else:
-  echo "FAIL"
-echo "Calculated total ", sum_match(t1.inp)
+for t in tests:
+    if sum_match(t.inp) == t.outp:
+        echo "PASS"
+    else:
+        echo "FAIL"
+        echo "Calculated total ", sum_match(t.inp)
