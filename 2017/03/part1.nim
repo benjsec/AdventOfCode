dd2925707ff2b61ee479430b0f0bb8efa8dbd618
Ben Secretan <github@benjsec.33mail.com>
2017-12-05 13:58:27 +0000

    Add solution to day 3 part 1
    This was quite hard to go through programatically, and the resulting
    code is quite messy to reflect that. It could almost certainly be
    improved, but does at least work.

--- 2017/03/part1.nim
+++ 2017/03/part1.nim
@@ -1,69 +1,88 @@
 import math
 
 
-# iterator odd_primes(): int {.closure.} =
-#     # Iterator to return all odd primes {1, 9, 25, ...}
-#     var i: int=1
-#     while true:
-#         yield i*i
-#         i+=2
-
-# proc get_ring(position: int): int =
-#     # Given a number in a spiral sequence, return the layer number
-#     var layer: int = 0
-#     for max_num in odd_primes():
-#         if max_num >= position:
-#             return layer
-#         layer += 1
+iterator odd_primes(): int {.closure.} =
+    # Iterator to return all odd primes {1, 9, 25, ...}
+    var i: int=1
+    while true:
+        yield i*i
+        i+=2
 
 proc get_ring(position: int): int =
     # Given a number in a spiral sequence, return the layer number
-    return int(math.floor(math.sqrt(float32(position)))/2.0)
+    var layer: int = 0
+    for max_num in odd_primes():
+        if max_num >= position:
+            return layer
+        layer += 1
 
 proc get_odd_prime(n: int): int =
     # Return the nth odd prime {1, 9, 25, ...}
     return ((2*n)-1) ^ 2
 
-proc spiral_mem_steps(position: int): int=
-    case position
-    of 1: return 0
-    of 2, 4, 6, 8: return 1
-    of 3, 5, 7, 9, 11, 15, 19, 23: return 2
-    of 10, 12, 14, 16, 18, 20, 24 #[et al]#: return 3
-    of 13, 17, 21, 25 #[ et al ]#: return 4
-    else: discard
-
-# Use examples given to test functions
-echo "Test 1: ", spiral_mem_steps(1)==0
-echo "Test 12: ", spiral_mem_steps(12)==3
-echo "Test 23: ", spiral_mem_steps(23)==2
-echo "Test 1024: ", spiral_mem_steps(1024)==31
-
-for i in 1..11:
-    echo i, " is in ring ", get_ring(i)
-echo 23, " is in ring ", get_ring(23)
-echo 1024, " is in ring ", get_ring(1024)
-echo "Max steps for ", 23, " is ", math.sqrt(float32(get_odd_prime(get_ring(23))))
-
-for i in 2..6:
-    echo get_odd_prime(i)-get_odd_prime(i-1)
-
-echo "Consider 22"
-echo "22 is in ring ", get_ring(22)
-var ring: int = get_ring(22)
-echo "22 is ", 22 - get_odd_prime(ring), "th in it's ring"
-
-# Use rotation to translate bottom half to top half
-
 proc rotate(position: int): int =
-    # If a position is in in the bottom `half` return the corresponding position from
-    # the `top` half
+    # If a position is not in first quarter return the corresponding position from
+    # the first quarter
     let ring_num: int = get_ring(position)
-    let num_in_ring: int = get_odd_prime(ring_num) - get_odd_prime(ring_num - 1)
-    if position - get_odd_prime(ring_num) > int(float32(num_in_ring)/2.0):
-        return position - int(float32(num_in_ring)/2.0)
-    else:
-        return position
+    let num_in_ring: int = get_odd_prime(ring_num+1) - get_odd_prime(ring_num)
+    var res: int = position-1
+    while res > num_in_ring div 4:
+        res -= num_in_ring div 4
+    return res
 
-echo "22 rotates to ", rotate(22) ## Should be 14
-echo "20 rotates to ", rotate(20) ## Should be 12
+proc dist_to_centre(position: int): int =
+    let ring_num: int = get_ring(position)
+    let num_in_ring: int = get_odd_prime(ring_num+1) - get_odd_prime(ring_num)
+    let centre: int = (num_in_ring div 8)
+    return abs(rotate(position)-centre)
+
+
+proc spiral_mem_steps(position: int): int=
+    return get_ring(position) + dist_to_centre(position)
+
+assert get_ring(1) == 0
+for i in 2..9:
+    if get_ring(i) != 1:
+        echo "Testing: ", i, " Expected 1 got ", get_ring(i)
+for i in 10.. 25:
+    if get_ring(i) != 2:
+        echo "Testing: ", i, " Expected 2 got ", get_ring(i)
+assert get_ring(26) == 3
+echo "Ring tests done"
+
+proc assert_eq(a: int, b: int) =
+    if a != b:
+        echo "Got ", a, " but expected ", b
+        assert false
+
+assert_eq(dist_to_centre(1), 0)
+assert_eq(dist_to_centre(2), 0)
+assert_eq(dist_to_centre(3), 1)
+assert_eq(dist_to_centre(4), 0)
+assert_eq(dist_to_centre(5), 1)
+assert_eq(dist_to_centre(6), 0)
+assert_eq(dist_to_centre(7), 1)
+assert_eq(dist_to_centre(8), 0)
+assert_eq(dist_to_centre(9), 1)
+assert_eq(dist_to_centre(10), 1)
+assert_eq(dist_to_centre(11), 0)
+assert_eq(dist_to_centre(12), 1)
+assert_eq(dist_to_centre(13), 2)
+assert_eq(dist_to_centre(26), 2)
+
+echo "Arm tests done"
+
+echo "1 ", spiral_mem_steps(1) == 0
+echo "2 ", spiral_mem_steps(2) == 1
+echo "6 ", spiral_mem_steps(6) == 1
+echo "5 ", spiral_mem_steps(5) == 2
+echo "9 ", spiral_mem_steps(9) == 2
+echo "15 ", spiral_mem_steps(15) == 2
+echo "19 ", spiral_mem_steps(19) == 2
+echo "10 ", spiral_mem_steps(10) == 3
+echo "18 ", spiral_mem_steps(18) == 3
+echo "13 ", spiral_mem_steps(13) == 4
+echo "25 ", spiral_mem_steps(25) == 4
+echo "1024 ", spiral_mem_steps(1024) == 31
+
+echo "Puzzle solution is ", spiral_mem_steps(312051), " steps"
