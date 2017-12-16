16e07fce577295af5bcb23542c01f130e6d68a3d
Ben S <github@benjsec.33mail.com>
2017-12-16 20:18:20 +0000

    Add solution to knot-hash puzzle

--- 2017/10/part1.nim
+++ 2017/10/part1.nim
@@ -1,31 +1,57 @@
-# Advent of Code Day 10 part 1 - hex grid
+# Advent of Code Day 10 part 1 - knot hash
 
+import algorithm
+import sequtils
 import strutils
-import math
 
-proc calc_steps(input: string): int =
+var 
+    list: seq[int]
+
+list = @[ 0, 1, 2, 3, 4 ]
+
+proc rotate[T](list: var seq[T], distance: int=1) =
+    var dist: int = distance.mod(len(list))
+    list = concat(list[low(list)+dist..high(list)],
+                  list[low(list)..<low(list)+dist])
+
+var test_l: seq[int] = @[1, 2, 3]
+test_l.rotate()
+assert test_l == @[2, 3, 1]
+test_l.rotate(2)
+assert test_l == @[1, 2, 3]
+test_l.rotate(2)
+assert test_l == @[3, 1, 2]
+test_l.rotate(3)
+assert test_l == @[3, 1, 2]
+
+proc twist[T](list: var seq[T], input: seq[int]) =
     var
-        north, west: int = 0
-    for step in input.split(","):
-        case step:
-            of "n":
-                north += 2
-            of "nw":
-                north += 1
-                west += 1
-            of "sw":
-                north -= 1
-                west += 1
-            of "s":
-                north -= 2
-            of "se":
-                north -= 1
-                west -= 1
-            of "ne":
-                north += 1
-                west -= 1
-            else:
-                discard
-        return abs(north) + abs(west)
+        rotated: int = 0
+        skip: int = 0
+    for length in input:
+        echo length
+        # echo "position $1   skip $2    finish $3" % [$position, $skip, $finish]
+        if length > 1:
+            echo "Reversing section $1" % [$list[0..<length]]
+            list.reverse(0, Natural(length-1))
+            echo list
+        echo "Rotating by $1" % [$length]
+        list.rotate(length+skip)
+        echo list
+        rotated += length+skip
+        skip += 1
+    list.rotate(len(list) - rotated.mod(len(list)))
 
-echo calc_steps("nw, nw, nw")
+list.twist(@[3, 4, 1, 5])
+echo list
+assert list[0] * list[1] == 12
+
+var
+    puzzle_array: seq[int] = @[]
+    puzzle_input: seq[int] = @[88,88,211,106,141,1,78,254,2,111,77,255,90,0,54,205]
+
+for x in 0..255:
+    puzzle_array.add(x)
+
+puzzle_array.twist(puzzle_input)
+echo puzzle_array[0] * puzzle_array[1]
